116230839a822ec017b0c6f167ae196914b853f1
Erik Terwan <info@komt-goed.com>
2015-02-04 00:15:17 +0100

    Initial Commit
    The first commit of FadeShow.

---
+++ README.md
@@ -0,0 +1,56 @@
+#FadeShow - A simple CSS3 slideshow
+After having done a lot of fading slideshow-backgrounds lately, i decided to make a jQuery plugin to make my life easier. It aspect-fills the images, so its perfect for backgrounds. You can set an option to preserve the aspect-fill while resizing.
+
+Enjoy, it might make your life easier as well.
+
+##Demo
+A live demo is available on [Github Pages](http://terwanerik.github.io/FadeShow).
+
+##Usage
+The plugin is really simple, add the jquery.fadeshow-[version].min.js to your project folder and include it.
+
+```
+<script src="src/jquery.fadeshow-0.1.min.js" type="text/javascript"></script>
+```
+
+Now add the CSS file and jquery.fadeshow-[version].min.css to your project and include it.
+
+```
+<link rel="stylesheet" href="css/jquery.fadeshow-0.1.min.css" />
+```
+
+Place a container for the slideshow anywhere on your page, a div is preferred (it needs a width and height to show).
+
+```
+<div class="slideshow"></div>
+```
+
+Now call the .fadeShow() function on that element, you need to pass an array of image-urls.
+
+```
+<script>
+$(function(){
+  $(".slideshow").fadeShow({
+    images: ['/img/one.jpg',
+	         '/img/two.jpg',
+	         '/img/three.jpg']
+  });
+});
+</script>
+```
+
+
+##Options
+There are a number of options you can pass to the fadeShow, if you need a preview on how to do that, check the [live demo](http://terwanerik.github.io/FadeShow).
+
+| Option Name | Type | Default | Description |
+| ----------- | ----------- |
+| images | Array of strings | null | All the images you want to display.  - REQUIRED |
+| speed | Integer | 8000 | The number of milliseconds to wait for the next slide to appear. |
+| startAtSlide | Integer | 0 | The index of the image to start with. |
+| correctRatio | Boolean | false | If true, the aspect-fill is kept intact while resizing the window. If false, the aspect-fill only works every time a new image appears. |
+| shuffle | Boolean | false | If true, the input will be shuffled, generating a random order every time you refresh. |
+
+
+##Known issues
+You can only change the fade speed / easing in the CSS file, somehow if you change it after the DOM has loaded, the CSS3 animation doesn't work.
---
+++ src/jquery.fadeshow-0.1.js
@@ -0,0 +1,134 @@
+/* 
+ * FadeShow jQuery Plugin
+ * Made by Erik Terwan
+ * 3 February 2015
+ * 
+ * This plugin is provided as-is and released under the terms of the MIT license.
+ */
+
+(function($){
+	$.fn.fadeShow = function(options){
+		/*
+		 *  VARIABLES
+		 */
+		
+		// Set the default options
+		var settings = $.extend({
+			images: null, //images (urls) to create the slideshow with, array of strings - required
+			speed: 8000, //milliseconds per slide, integer
+			startAtSlide: 0, //the slide index to start the slideshow with, integer
+			correctRatio: false, //if true, the aspect fill will be intact even when resizing the window, boolean
+								//only handy for full width / height slideshows, otherwise slows your page down
+			shuffle: false //if true, the slides will be shuffled before shown, get a unique slider each refresh, boolean
+		}, options);
+		
+		var $thisElement = this; //for use inside functions etc.
+		var $imageElements = new Array(); //all the image elements in the slider
+		var currentSlide = settings.startAtSlide; //the current slide
+		
+		/*
+		 *  PRIVATE METHODS
+		 */
+		
+		//initialize the slideshow
+		function init()
+		{
+			$thisElement.append("<div class='fadeShow-container'></div>"); //add the container to the element
+			
+			$.each(settings.images, function(count){
+				$(".fadeShow-container", $thisElement).append(format(this, count)); //format the image urls and place them inside the container
+				
+				var $imageElement = $("#fadeShow-slide"+count); //get the object
+				
+				$imageElements.push($imageElement); //add it to the array of elements
+			});
+			
+			nextImage(); //set the image for 'startAtSlide' option
+			
+			setInterval(nextImage, settings.speed); //set the timer
+			
+			//check the settings for alterations
+			if(settings.correctRatio){
+				//correct the fill ratio when rezing the window 
+				$(window).resize(function(){
+					var runningElement = currentSlide - 1; //get the current visible slide,
+					//that is currentSlide minus one because we upped it with 1 in the nextImage function
+					
+					if(runningElement < 0){
+						runningElement = $imageElements.length - 1; //lower than 0 == last slide
+					}
+					
+					setImageRatio($imageElements[runningElement]); //set the ratio
+				});
+			}
+			
+			if(settings.shuffle){
+				//shuffle the images
+				$imageElements = shuffleArray($imageElements);
+			}
+			
+			return $thisElement; //return self for chaining
+		}
+		
+		//format the image URL to an image element
+		function format(image, count)
+		{
+			//using the background-image, but adding the image element so we can get the aspect ratio
+			return "<div class='image' id='fadeShow-slide"+count+"' style='background-image: url("+image+");'><img src='"+image+"' alt='' /></div>";
+		}
+		
+		//display the next image in the array
+		function nextImage()
+		{
+			//loop through all image elements
+			$.each($imageElements, function(count){
+				if(currentSlide == count){
+					setImageRatio(this);
+					
+					$(this).addClass('active').removeClass('inactive'); //toggle active class
+				} else{
+					$(this).removeClass('active').addClass('inactive'); //toggle inactive class 
+				}
+			});
+			
+			currentSlide++; //up the currentSlide by 1
+			
+			if(currentSlide >= $imageElements.length){
+				currentSlide = 0; //if bigger than number of images start at the beginning
+			}
+		}
+		
+		//set the correct aspect fill ratio for an image element
+		function setImageRatio($imageElement)
+		{
+			var parentRatio = parseInt($thisElement.width()) / parseInt($thisElement.height());
+			var imageRatio = 100 / parseInt($("img", $imageElement).height());
+			
+			if(imageRatio > parentRatio){
+				//fill the image using 100% height
+				$imageElement.addClass('fillHeight');
+			} else{
+				//fill image using 100% width
+				$imageElement.removeClass('fillHeight');
+			}
+		}
+		
+		return init(); //return init > this for chaining
+	};
+}(jQuery));
+
+/**
+ * Randomize array element order in-place.
+ * Using Fisher-Yates shuffle algorithm.
+ * From http://stackoverflow.com/a/12646864/886926
+ */
+function shuffleArray(array)
+{
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
